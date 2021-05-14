bc9a01188476a341929d14801005e57859c44f24
Nemanja Mijucic <nemanjamijucic@Nemanjas-MacBook-Pro.local>
2021-05-14 08:52:36 +0200

    updated

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ window.addEventListener('load', () => {
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationCountry = document.querySelector('.location-country');
     let locationTimezone = document.querySelector('.location-timezone');
     let icon = document.getElementById('icon');
     let feelsLike = document.querySelector('.temperature-feelsLike');
+    let windSpeed = document.querySelector('.wind-speed');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2c4ae810fb0582e5bfd2d6d93d8e708d`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2c4ae810fb0582e5bfd2d6d93d8e708d`;
 
             fetch(api)
                 .then(response => {
@@ -23,10 +25,12 @@ window.addEventListener('load', () => {
                     const description = data.weather[0].description;
                     temperatureDescription.innerText = description;
                     const temperature = Math.floor(data.main.temp - 273);
-                    temperatureDegree.innerText = temperature;
-                    locationTimezone.innerText = data.name;
+                    temperatureDegree.innerText = `temperature: ${temperature} C`;
+                    locationTimezone.innerText =  data.name;
+                    locationCountry.innerText = data.sys.country;
                     icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-                    feelsLike.innerText = `feels like ${Math.round(data.main.feels_like - 273)}`;
+                    feelsLike.innerText = `feels like: ${Math.round(data.main.feels_like - 273)} C`;
+                    windSpeed.innerText = `wind speed: ${data.wind.speed} m/s`;
                     console.log(data);
                 })
 
@@ -40,4 +44,3 @@ window.addEventListener('load', () => {
 
 
 });
-
